65f5223968afc20960f29d04562a82e8ef619ba1
TimPietrusky <timpietrusky@gmail.com>
2014-08-27 01:17:52 +0200

    Added more gulp action.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
     notify = require('gulp-notify'),
-    zip = require('gulp-zip')
+    zip = require('gulp-zip'),
+    htmlreplace = require('gulp-html-replace'),
+    htmlmin = require('gulp-htmlmin')
 ;
 
 
@@ -14,9 +16,10 @@ var gulp = require('gulp'),
 
 // optionss and names and stuff
 var options = {
-  js: [ 'assets/js/*.js' ],
+  js_files : [ 'assets/js/car.js', 'assets/js/player.js', 'assets/js/points.js', 'assets/js/draw.js', 'assets/js/init.js' ],
   jsmin : 'roadthegame.js',
   css : [ 'assets/css/style.css'],
+  html : 'index.html',
   compile : 'compile',
   build : 'build',
   archive : 'roadthegame.zip',
@@ -29,7 +32,7 @@ var options = {
 
 // Create the minified version of all JS files
 gulp.task('js', function() {
-  return gulp.src(options.js)
+  return gulp.src(options.js_files)
     .pipe(concat(options.jsmin))
     .pipe(rename({suffix: '.min'}))
     .pipe(uglify())
@@ -54,9 +57,29 @@ gulp.task('css', function() {
 
 
 
+// Replace blocks and minifiy the HTML
+gulp.task('html', function() {
+  return gulp.src(options.html)
+    // Replace blocks with new code
+    .pipe(htmlreplace({
+        'css': 'style.min.css',
+        'js': 'roadthegame.min.js'
+    }))
+    // Minify the HTML
+    .pipe(htmlmin({
+        collapseWhitespace: true,
+        removeComments : true
+    }))
+    .pipe(gulp.dest(options.compile))
+  ;
+});
+
+
+
+
 
 // Calls other tasks and creates the archive
-gulp.task('build', [ 'js', 'css' ], function() {
+gulp.task('build', [ 'js', 'css', 'html' ], function() {
     return gulp.src(options.archive_content)
       .pipe(zip(options.archive))
       .pipe(gulp.dest(options.build))
